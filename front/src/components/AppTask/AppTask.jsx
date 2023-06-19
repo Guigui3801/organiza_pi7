c1058ea08f009f638ada7176cf9bac525803f5d6
Guilherme Souza Reis <gsrmlopes@gmail.com>
2023-06-19 10:59:36 -0300

    Fix nas requisições

--- front/src/components/AppTask/AppTask.jsx
+++ front/src/components/AppTask/AppTask.jsx
@@ -4,6 +4,46 @@ import { Button, Menu, Input } from "antd";
 import { CreateTask } from "./AppTask.Service";
 
 const taskData = [
+    {
+        id: 1,
+        name: "Exemplo Concluida",
+        created_at: "10/10/2021",
+        completed: true,
+        postponed: false,
+        forgot: false,
+        abandoned: false,
+        forbidden: false,
+    },
+    {
+        id: 2,
+        name: "Exemplo Abandonada",
+        created_at: "10/10/2021",
+        completed: false,
+        postponed: false,
+        forgot: false,
+        abandoned: true,
+        forbidden: false,
+    },
+    {
+        id: 3,
+        name: "Exemplo incompleta",
+        created_at: "10/10/2021",
+        completed: false,
+        postponed: false,
+        forgot: false,
+        abandoned: false,
+        forbidden: false,
+    },
+    {
+        id: 4,
+        name: "Exemplo Proibida",
+        created_at: "10/10/2021",
+        completed: false,
+        postponed: false,
+        forgot: false,
+        abandoned: false,
+        forbidden: true,
+    }
 
 ];
 
@@ -21,7 +61,7 @@ const AppTask = () => {
     }
     const handleAppend = () => {
         if (inputValue != '') {
-            CreateTask(inputValue);            
+            CreateTask(inputValue);
             setInputValue('');
         }
         setMenuOpen(!menuOpen);
@@ -29,63 +69,42 @@ const AppTask = () => {
 
     return (
         <div className="AppTask">
-
             <div className="container">
                 <h2>Tarefas</h2>
                 <Button className="addTask" type="default" onClick={handleAdd}>
                     Adicionar tarefa
                 </Button>
-                <Menu mode="inline" theme="dark" className='addMenu' hidden={!menuOpen}>
+                <Menu mode="inline" theme="dark" className={`addMenu ${menuOpen ? '' : 'hidden'}`}>
                     <Menu.Item key="1">
                         <Input placeholder="Nome da tarefa" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
                     </Menu.Item>
-                    <Menu.Item key="2"><Button onClick={() => handleAppend()}>Adicionar</Button></Menu.Item>
+                    <Menu.Item key="2">
+                        <Button onClick={handleAppend}>Adicionar</Button>
+                    </Menu.Item>
                 </Menu>
                 <div className="spacer" />
                 {/* button */}
                 <Button className="showTasks" type="default" onClick={handleTaks}>
-                    <h3>
-                        Suas Tarefas :
-                    </h3>
+                    <h3>Suas Tarefas :</h3>
                 </Button>
                 <div className="spacer" />
-                <Menu
-                    mode="inline"
-                    style={{
-                        backgroundColor: 'transparent',
-                    }}
-                    className="showTasks"
-                    hidden={!menuTarefasOpen}
-                >
+                <Menu mode="inline" className={`showTasks ${menuTarefasOpen ? '' : 'hidden'}`}>
                     {taskData.length === 0 ? (
-                        <p>Todas as tarefas concluídas!</p>
+                        <p className="Tasks">Todas as tarefas concluídas!</p>
                     ) : (
                         taskData.map((task) => (
-                            <div key={task.id}>
-                                <p
-                                    style={{
-                                        paddingBottom: '25px',
-                                        color:
-                                            task.completed
-                                                ? 'green'
-                                                : task.postponed ||
-                                                    task.forgot ||
-                                                    task.abandoned ||
-                                                    task.forbidden
-                                                    ? 'red'
-                                                    : 'black',
-                                    }}
-                                >
-                                    {task.name} =&gt; Criada em =&gt; {task.created_at}
+                            <div key={task.id} className="Tasks">
+                                <p className="TaskColor">
+                                    {task.name}
                                 </p>
                                 <br />
+                                <p className={`TaskDate ${task.completed ? 'completed' : task.postponed || task.forgot || task.abandoned ? 'postponed' : task.forbidden ? 'forbidden' : ''}`}>
+                                    {task.created_at}
+                                </p>
                             </div>
                         ))
                     )}
                 </Menu>
-
-
-
             </div>
         </div>
     );
