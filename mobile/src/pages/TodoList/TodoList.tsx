26b326d5fb9f9323f0b9d0ed1ed86325d8b85e3e
Guigui3801 <79230328+Guigui3801@users.noreply.github.com>
2023-06-19 07:57:58 -0300

    Mudanças feitas no pomodoro do app

--- mobile/src/pages/TodoList/TodoList.tsx
+++ mobile/src/pages/TodoList/TodoList.tsx
@@ -1,19 +1,33 @@
 import { View, StyleSheet, ScrollView } from "react-native";
 import Checkbox from "expo-checkbox";
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import TaskCard from "../../components/TaskCard";
 import { Button } from "react-native-paper";
-
+import { AuthContext } from "../../contexts/AuthContext";
+import { fetchUserUnitTasks } from "../../services/unitTask";
 export default function TodoList() {
+
+useEffect(() => {
+  getAllTasks();
+},[])
+
+async function getAllTasks(){
+  await fetchUserUnitTasks().then((resp) =>{
+console.log(resp)
+  }
+  )
+}
+
+
   return (
-    <ScrollView style={{ flex: 1, marginTop: 30}}>
-      <TaskCard name="Lavar a louça" created_at="20/05/2023" completed={true} status="Não feita" />
-      <TaskCard name="Limpar o quarto" created_at="13/06/2023" completed={true} status="Não feita" />
-      <TaskCard name="Lavar a louça" created_at="14/05/2002" completed={true} status="Não feita" />
-      <TaskCard name="Lavar a louça" created_at="17/05/2002" completed={true} status="Não feita" />
-      <TaskCard name="Lavar a louça" created_at="18/05/2002" completed={true} status="Não feita" />
-      <TaskCard name="Lavar a louça" created_at="10/05/2002" completed={true} status="Não feita" />
-      <TaskCard name="Lavar a louça" created_at="12/06/2002" completed={true} status="Não feita" />
+    <ScrollView style={{ flex: 1, marginTop: 30 }}>
+
+      <TaskCard
+        name="Lavar a louça"
+        created_at="20/05/2023"
+        completed={true}
+        status="Não feita"
+      />
     </ScrollView>
   );
 }
--- mobile/src/services/unitTask.ts
+++ mobile/src/services/unitTask.ts
@@ -1,8 +1,12 @@
+import { AuthContext } from "../contexts/AuthContext";
 import { api } from "./api";
-
+import React, {useContext} from 'react';
 const fetchUserUnitTasks = async () => {
+const {user} = useContext(AuthContext)
+
   try {
-    const response = await api.get("/userUnitTasks");
+    const response = await api.get(`/userUnitTasks/${user.id}`,
+    );
     return response.data;
   } catch (error) {
     console.log("Erro ao buscar:", error);
